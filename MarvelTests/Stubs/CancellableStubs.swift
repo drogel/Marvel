b5e9a6c622fd9f9e10890e84dea77fce5e34ac0f
Diego Rogel <diegorogel3d@hotmail.com>
2022-01-23 11:55:17 +0100

    feat: adds character detail use case

---
+++ MarvelTests/Stubs/CancellableStubs.swift
@@ -0,0 +1,14 @@
+//
+//  CancellableStubs.swift
+//  MarvelTests
+//
+//  Created by Diego Rogel on 23/1/22.
+//
+
+import Foundation
+@testable import Marvel_Debug
+
+class CancellableStub: Cancellable {
+
+    func cancel() { }
+}
