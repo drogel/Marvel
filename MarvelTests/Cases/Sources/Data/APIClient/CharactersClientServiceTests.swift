46f68a61b122ebe869d9c70421fc809f0a9feb3f
Diego Rogel <diegorogel3d@hotmail.com>
2022-01-29 18:37:17 +0100

    refactor: adds test coverage to CharactersClientService

--- MarvelTests/Cases/Sources/Data/APIClient/CharactersClientServiceTests.swift
+++ MarvelTests/Cases/Sources/Data/APIClient/CharactersClientServiceTests.swift
@@ -32,13 +32,72 @@ class CharactersClientServiceTests: XCTestCase {
         whenRetrievingCharactersIgnoringResult()
         assertNetworkServiceRequest(callCount: 1)
     }
+
+    func test_givenASucessfulNetworkServiceAndFailingParser_whenRetrievingCharacters_resultIsFailure() {
+        givenFailingParser()
+        givenSutWithSuccessfulNetworkService()
+        let result = whenRetrievingCharacters()
+        assertIsFailure(result) {
+            assert($0, isEqualTo: .emptyData)
+        }
+    }
+
+    func test_givenASucessfulNetworkServiceAndSuccessfulParser_whenRetrievingCharacters_resultIsSuccess() {
+        givenSuccesfulParser()
+        givenSutWithSuccessfulNetworkService()
+        let result = whenRetrievingCharacters()
+        assertIsSuccess(result) {
+            XCTAssertEqual($0, Self.dataWrapperResponseStub)
+        }
+    }
+
+    func test_givenNoConnection_whenRetrievingCharacters_resultIsFailureWithNoConnectionError() {
+        assertWhenRetrievingCharacters(
+            returnsFailureWithError: .noConnection,
+            whenNetworkErrorWas: .notConnected
+        )
+    }
+
+    func test_givenNoAuthentication_whenRetrievingCharacters_resultIsFailureWithUnauthorized() {
+        assertWhenRetrievingCharacters(
+            returnsFailureWithError: .unauthorized,
+            whenNetworkErrorWas: .unauthorized
+        )
+    }
+
+    func test_givenGenericError_whenRetrievingCharacters_resultIsFailureWithEmptyData() {
+        assertWhenRetrievingCharacters(
+            returnsFailureWithError: .emptyData,
+            whenNetworkErrorWas: .statusCodeError(statusCode: 500)
+        )
+    }
 }
 
 private extension CharactersClientServiceTests {
 
+    static let dataWrapperResponseStub = DataWrapper.withNilData
+
     func givenSutWithNetworkServiceMock() {
         networkServiceMock = NetworkServiceMock()
         givenSut(with: networkServiceMock)
+    }
+
+    func givenSutWithSuccessfulNetworkService() {
+        networkServiceMock = NetworkServiceSuccessfulStub()
+        givenSut(with: networkServiceMock)
+    }
+
+    func givenSutWithFailingNetworkService(providingError error: NetworkError) {
+        networkServiceMock = NetworkServiceFailingStub(errorStub: error)
+        givenSut(with: networkServiceMock)
+    }
+
+    func givenFailingParser() {
+        jsonParserMock = JSONParserFailingStub()
+    }
+
+    func givenSuccesfulParser() {
+        jsonParserMock = JSONParserSuccessfulStub<DataWrapper>(dataStub: Self.dataWrapperResponseStub)
     }
 
     func givenSut(with networkService: NetworkService) {
@@ -59,5 +118,15 @@ private extension CharactersClientServiceTests {
 
     func assertNetworkServiceRequest(callCount: Int, line: UInt = #line) {
         XCTAssertEqual(networkServiceMock.requestCallCount, callCount, line: line)
+    }
+
+    func assert(_ actualError: CharactersServiceError, isEqualTo expectedError: CharactersServiceError) {
+        if case actualError = expectedError { } else { XCTFail() }
+    }
+
+    func assertWhenRetrievingCharacters(returnsFailureWithError expectedError: CharactersServiceError, whenNetworkErrorWas networkError: NetworkError, line: UInt = #line) {
+        givenSutWithFailingNetworkService(providingError: networkError)
+        let result = whenRetrievingCharacters()
+        assertIsFailure(result, then: { assert($0, isEqualTo: expectedError) }, line: line)
     }
 }
