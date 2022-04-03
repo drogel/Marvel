e8903e47a2f4aae7112ecded84441002121c226a
Diego Rogel <diegorogel3d@hotmail.com>
2022-04-03 17:11:22 +0200

    refactor(async-await): moves fetch image view to async await

--- Marvel/Sources/Data/APIClient/Network/NetworkSession.swift
+++ Marvel/Sources/Data/APIClient/Network/NetworkSession.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-typealias NetworkCompletion = (Data?, URLResponse?, Error?) -> Void
-
 protocol NetworkSession {
     func loadData(from request: URLRequest) async throws -> (data: Data, response: URLResponse)
 }
