7d9410a4415031a9f06cb6498276ca9e6779c5da
Diego Rogel <diegorogel3d@hotmail.com>
2022-07-23 12:55:41 +0200

    feat(swiftui): creates CellStyle

--- Marvel/Sources/Presentation/Sources/Presentation/Characters/SwiftUIViews/CharacterCellView.swift
+++ Marvel/Sources/Presentation/Sources/Presentation/Characters/SwiftUIViews/CharacterCellView.swift
@@ -18,20 +18,17 @@ struct CharacterCellView: View {
         ZStack(alignment: .bottom) {
             AsyncImage(url: model.imageURL)
                 .frame(width: 0, height: 300, alignment: .center)
-            ZStack(alignment: .bottom) {
+            ZStack {
                 Rectangle()
                     .foregroundColor(.white)
                     .opacity(0.93)
-                VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: -24) {
                     Text(model.name)
                         .fontWeight(.bold)
                         .lineLimit(1)
                         .multilineTextAlignment(.leading)
                         .font(.title2)
-                        .padding(.top)
-                        .padding(.leading)
-                        .padding(.bottom, model.description.isEmpty ? nil : 1)
-                        .padding(.trailing)
+                        .padding()
                         .frame(maxWidth: .infinity, alignment: .leading)
                     if !model.description.isEmpty {
                         Text(model.description)
@@ -39,16 +36,13 @@ struct CharacterCellView: View {
                             .font(.caption)
                             .foregroundColor(.gray)
                             .lineLimit(2)
-                            .padding(.leading)
-                            .padding(.bottom)
-                            .padding(.trailing)
+                            .padding()
                             .frame(maxWidth: .infinity, alignment: .leading)
                     }
                 }
             }
             .fixedSize(horizontal: false, vertical: true)
         }
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-        .shadow(color: .black.opacity(0.15), radius: 10, x: 0, y: 6)
+        .cellStyle(.big)
     }
 }
